0e213c56d85f2781d9a4f7258aea68ae88dc97f3
Maheshgopathi <84897692+Maheshgopathi@users.noreply.github.com>
2022-10-06 12:10:39 +0530

    Add files via upload

---
+++ Logincatalyst/Logincatalyst/LogincatalystApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogincatalystApp.swift
+//  Logincatalyst
+//
+//  Created by Gopathi Mahesh on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LogincatalystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
